89941e3f00c45c56935e3f4db49dabd841dd57c9
mooodiii <midokanta.23@outlook.com>
2021-05-25 19:37:21 +0100

    pushing interface build

---
+++ gestionBarage/interface/build/precache-manifest.eb620391c453cb20ce25407fc6519e59.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b98aeb600d31db676cdc355942bc85c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f133cadfa8aa31c82481",
+    "url": "/static/css/2.c114f9e7.chunk.css"
+  },
+  {
+    "revision": "53fed6cc949e51e14b86",
+    "url": "/static/css/main.60a7b5a9.chunk.css"
+  },
+  {
+    "revision": "f133cadfa8aa31c82481",
+    "url": "/static/js/2.ab3de973.chunk.js"
+  },
+  {
+    "revision": "f24bbe415d04c15ec599bd0656700bd8",
+    "url": "/static/js/2.ab3de973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53fed6cc949e51e14b86",
+    "url": "/static/js/main.bc5f74de.chunk.js"
+  },
+  {
+    "revision": "476fec442afe5b76bad1",
+    "url": "/static/js/runtime-main.0e51c17f.js"
+  },
+  {
+    "revision": "12cc543d23e7189838571cb215fc4d3e",
+    "url": "/static/media/logo.12cc543d.png"
+  }
+]);
